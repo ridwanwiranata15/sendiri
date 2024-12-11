f48d4af3273e773dbd49f5d9b30f1073f518287e
ridwanwiranata15 <jagokoding15@gmail.com>
2024-12-11 17:14:06 +0700

    assalamualaikum

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,9 @@
+<script src="../assets/js/color-modes.js"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.122.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Chart.js -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
